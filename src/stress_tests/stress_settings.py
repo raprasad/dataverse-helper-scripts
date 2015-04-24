fef401a68723bb778b815d9db375a3fd996a5050
Raman Prasad <raman_prasad@harvard.edu>
2015-04-24 16:52:52 -0400

    update

--- src/stress_tests/stress_settings.py
+++ src/stress_tests/stress_settings.py
@@ -12,3 +12,30 @@ HELPER_SCRIPTS_DIR = join(dirname(STRESS_TEST_DIR))#, 'helper_utils')
 sys.path.append(HELPER_SCRIPTS_DIR)
 print HELPER_SCRIPTS_DIR
 from helper_utils.msg_util import *
+
+
+
+
+# ----------------------------
+# Any needed credentials -
+#  stored in a JSON file
+# ----------------------------
+
+CREDS_FNAME = join(STRESS_TEST_DIR, 'creds.json')
+CREDS_LOOKUP = {}
+def get_creds_info(key_name):
+    global CREDS_LOOKUP
+    assert isfile(CREDS_FNAME), 'File not found: %s' % CREDS_FNAME
+
+    if CREDS_LOOKUP.has_key(key_name):
+        return CREDS_LOOKUP.get(key_name)
+
+    json_creds = json.loads(open(CREDS_FNAME, 'r').read())
+
+    assert json_creds.has_key(key_name), 'Key *%s* not found in creds file' % key_name
+
+    cred_value = json_creds.get(key_name)
+
+    CREDS_LOOKUP[key_name] = cred_value
+
+    return cred_value
--- src/stress_tests/test_01.py
+++ src/stress_tests/test_01.py
@@ -2,7 +2,10 @@ from stress_settings import *
 
 import os, sys
 import sys
+import random, string
+from random import randint
 from locust import HttpLocust, TaskSet, task
+from bs4 import BeautifulSoup
 
 class UserBehavior(TaskSet):
     def on_start(self):
@@ -11,21 +14,46 @@ class UserBehavior(TaskSet):
         #self.login()
 
 
-    @task(0)
+    #@task(1)
     def login(self):
-        r = self.client.post('/loginpage.xhtml?redirectPage=/dataverse.xhtml'\
-                        , {"loginForm:credentialsContainer2:0:credValue":"admin"
-                        , "loginForm:credentialsContainer2:1:sCredValue":"admin"\
-                        #, "loginForm:loginSystemSelect" : "builtin"\
+        msg('> login')
+        random_user = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        random_pass = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+        r = self.client.post('/loginpage.xhtml?redirectPage=/dataverse.xhtml'
+                        , {"loginForm:credentialsContainer2:0:credValue": random_user
+                        , "loginForm:credentialsContainer2:1:sCredValue": random_pass
                         , "loginForm" : "loginForm"\
                         })
         print 'status code: %s' % r.status_code
         print 'LOGIN SUCCESS :):):)'
 
-    @task(2)
+    @task(1)
     def homepage(self):
         msg('> Go to homepage. Locust instance (%s)' % self.locust)
-        self.client.get("/")
+        r = self.client.get("/")
+        """
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.text)
+            links = soup.find_all('a')
+            dataset_links = [ l for l in links if l.get('href', '').find('dataset.xhtml') > -1 ]
+            if len(dataset_links) > 0:
+                rand_idx = randint(0, len(dataset_links)-1)
+                selected_link = dataset_links[rand_idx]
+                msgt('Random dataset link: %s' % selected_link)
+                self.client.get(selected_link.get('href'))
+        """
+
+    @task(1)
+    def random_vm5_dataset(self):
+        ids = """29669 55LJ9P 6AOL3H DDDOKP MPU019 OJZKKP VZLVVI ZJNG1N""".split()
+        rand_idx = randint(0, len(ids)-1)
+        selected_id = ids[rand_idx]
+        
+        ds_url = '/dataset.xhtml?persistentId=doi:10.7910/DVN/%s' % selected_id
+        
+        self.client.get(ds_url)
+        
+            
 
     @task(1)
     def profile_page(self):
@@ -34,8 +62,8 @@ class UserBehavior(TaskSet):
         
 
 class WebsiteUser(HttpLocust):
-    host = 'https://dvn-build.hmdc.harvard.edu'
-    #host = 'https://dataverse-demo.iq.harvard.edu'
+    #host = 'https://dvn-build.hmdc.harvard.edu'
+    host = get_creds_info('SERVER')
     task_set = UserBehavior
     min_wait=5000
-    max_wait=9000
+    max_wait=20000
